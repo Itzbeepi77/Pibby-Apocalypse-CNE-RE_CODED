c32dd5852adeaa5742c65b653eab633906041b2e
Itz_beepi77 <aabiynugraha@gmail.com>
2024-06-05 20:15:16 +0700

    another commit ye

--- assets/data/events/defaultZoomTween.hx
+++ assets/data/events/defaultZoomTween.hx
@@ -2,6 +2,6 @@ function onEvent(eventEvent) {
   var params:Array = eventEvent.event.params;
   if (eventEvent.event.name == "defaultZoomTween") {
     var flxease:String = params[2] + (params[2] == "linear" ? "" : params[3]);
-    FlxTween.num(defaultCamZoom, params[0], params[1], {ease: Reflect.field(FlxEase, flxease), function(value) { defaultCamZoom = value; }});
+    FlxTween.num(defaultCamZoom, params[0], params[1], {ease: Reflect.field(FlxEase, flxease)}, function(value) { defaultCamZoom = value; });
   }
 }
