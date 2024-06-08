91257026e7804926057a6dd32b38fe35d9dd4213
Notbeep-,- <112723428+Itzbeepi77@users.noreply.github.com>
2024-06-09 03:42:29 +0700

    Update windowTitleSongs.hx

--- assets/songs/windowTitleSongs.hx
+++ assets/songs/windowTitleSongs.hx
@@ -9,7 +9,7 @@ function postCreate(){
             window.title = windowTitle + " - By Kylevi";
         case "suffering-siblings":
             window.title = windowTitle + " - By Awe (Ft. Saster & Shxdow)";
-        case "come-along-with-me", "forgotten-world", "my-finale":// will add child's play v2 later if I want to
+        case "come-along-with-me", "forgotten-world":// will add child's play v2 and my finale later if I want to
             window.title = windowTitle + " - By Awe";
         case "childs-play":
             window.title = windowTitle + " - By Yoosuf Meekail";
@@ -21,12 +21,5 @@ function postCreate(){
             window.title = windowTitle + " - By Spades (Ft. Stormy & Myakish)";
         case "no-hero-remix":
             window.title = windowTitle + " - By RunDown";
-            
-        // moved/split, this song is gonna be it's own playable mod (like oneshot or bonus/extra update or smth)
-        /*case "what-am-i":
-            window.title = windowTitle + " - By IZincl";
-        
-        case "my-doll":
-            window.title = windowTitle + " - By End-Sella (Ft. DemonStep)";*/
     }
 }
